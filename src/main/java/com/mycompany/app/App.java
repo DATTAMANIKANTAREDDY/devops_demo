806e72045a336eaac2d82b379d98376a192d7cdd
manikantareddy <bmanikantareddy01@gmail.com>
2023-08-29 15:16:48 +0530

    second commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "wellcome to DIET school" );
+        System.out.println( "arey enti ra edi........." );
     }
 }
